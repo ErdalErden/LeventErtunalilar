11bd0a4f1ec260f0cf1ee4a120e9d44eed2e75ee
ErdalErden <you@example.com>
2022-04-12 23:09:45 +0300

    Çalışıldı

--- GaleriApp/project.js
+++ GaleriApp/project.js
@@ -9,22 +9,32 @@ const ui = new UI();
 // Tüm eventleri yükleme
 eventListeners();
 
-function eventListeners(){ 
+function eventListeners() {
     carform.addEventListener("submit", addCar);
 }
 
-function addCar(e){
-    const title = titleElement.value;
+function addCar(e) {
+    const title = titleElement.value.toUpperCase();
     const price = priceElement.value;
     const url = urlElement.value;
 
-    if(title === "" || price === "" || url === ""){
-        //Hata
+    if(ui.titleCheck(title) == false) {
+        ui.displayMessages("Başlık Bilgisi Geçersiz","danger");
+        ui.odak(titleElement);
+    }else if(ui.priceCheck(price) == false) {
+        ui.displayMessages("Fiyat Bilgisi Geçersiz","danger");
+        ui.odak(priceElement);
+    }else if(ui.urlCheck(url) == false) {
+        ui.displayMessages("Resim Bilgisi Geçersiz","danger");
+        ui.odak(urlElement);  
     }
-    else{
-        //Yeni Araç
+    else {
         const newCar = new Car(title,price,url);
         ui.addCarToUI(newCar);//arayüze araç ekleme  
+        ui.clearInputs(titleElement, priceElement, urlElement);
+        ui.displayMessages("Araç başarıyla eklendi ...","success");
     }
-    e.preventDefault();    
+    e.preventDefault();
 }
+
+
--- GaleriApp/ui.js
+++ GaleriApp/ui.js
@@ -25,3 +25,50 @@ UI.prototype.addCarToUI = function (newCar) {
     </tr>    
     `
 }
+
+UI.prototype.clearInputs = function (element1, element2, element3) {
+    element1.value = "";
+    element2.value = "";
+    element3.value = "";
+}
+
+UI.prototype.titleCheck = function (title) {
+    let result = true;
+    if (title === "") {
+        result = false;
+    }
+    return result;
+}
+
+UI.prototype.priceCheck = function (price) {
+    let result = true;
+    if (price === "") {
+        result = false;
+    }
+    return result;
+}
+UI.prototype.urlCheck = function (url) {
+    let result = true;
+    if (url === "") {
+        result = false;
+    }
+    return result;
+}
+
+UI.prototype.displayMessages = function (message, type) {
+    const cardBody = document.querySelector(".card-body");
+    //Alert Divini oluşturma
+    const div = document.createElement("div");
+    div.className = `alert alert-${type}`;
+    div.textContent = message;
+
+    cardBody.appendChild(div);
+    setTimeout(function () {
+        div.remove();
+    }, 2000)
+}
+
+UI.prototype.odak = function (control) {
+    control.focus();
+    control.select();
+}
